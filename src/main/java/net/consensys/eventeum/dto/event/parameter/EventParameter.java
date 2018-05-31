a46f3742bc3ea47b41ccd55618c8e812a001eb5c
Craig <craigwilliams84@googlemail.com>
2018-05-31 13:42:49 +0100

    Added multi instance docker and changed the the way that EventParemeter is parsed.

--- src/main/java/net/consensys/eventeum/dto/event/parameter/EventParameter.java
+++ src/main/java/net/consensys/eventeum/dto/event/parameter/EventParameter.java
@@ -1,6 +1,8 @@
 package net.consensys.eventeum.dto.event.parameter;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 /**
@@ -11,9 +13,17 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
  * @author Craig Williams <craig.williams@consensys.net>
  */
 @JsonTypeInfo(
-        use = JsonTypeInfo.Id.CLASS,
-        include = JsonTypeInfo.As.PROPERTY,
-        property = "class")
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "type",
+        visible = true)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = StringParameter.class, name = "address"),
+        @JsonSubTypes.Type(value = StringParameter.class, name = "bytes32"),
+        @JsonSubTypes.Type(value = StringParameter.class, name = "string"),
+        @JsonSubTypes.Type(value = NumberParameter.class, name = "uint256")
+})
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public interface EventParameter<T> {
     String getType();
 
