baf87e422d8893d00c447ac3b01daa085f55b90e
Craig <craigwilliams84@googlemail.com>
2019-07-16 14:39:24 +0100

    Fixed Dockerfile to allow config path specification

---
+++ server/docker-scripts/start-eventeum.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+command="java -jar eventeum-server.jar"
+if [[ -z CONF ]]; then
+  command="$command --spring.config.additional-location=$CONF"
+fi
+
+echo "Starting eventeum with command: $command"
+eval $command
