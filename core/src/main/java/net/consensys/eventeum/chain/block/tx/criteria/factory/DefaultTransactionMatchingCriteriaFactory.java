d81eea4a69707a6de26fb6f7c6a88b77a008e94a
janayama <javier.anaya.martinez@gmail.com>
2019-07-17 19:44:34 +0200

    add new types to factory

--- core/src/main/java/net/consensys/eventeum/chain/block/tx/criteria/factory/DefaultTransactionMatchingCriteriaFactory.java
+++ core/src/main/java/net/consensys/eventeum/chain/block/tx/criteria/factory/DefaultTransactionMatchingCriteriaFactory.java
@@ -1,5 +1,7 @@
 package net.consensys.eventeum.chain.block.tx.criteria.factory;
 
+import net.consensys.eventeum.chain.block.tx.criteria.FromAddressMatchingCriteria;
+import net.consensys.eventeum.chain.block.tx.criteria.ToAddressMatchingCriteria;
 import net.consensys.eventeum.chain.block.tx.criteria.TransactionMatchingCriteria;
 import net.consensys.eventeum.chain.block.tx.criteria.TxHashMatchingCriteria;
 import net.consensys.eventeum.model.TransactionIdentifierType;
@@ -12,7 +14,15 @@ public class DefaultTransactionMatchingCriteriaFactory implements TransactionMat
     @Override
     public TransactionMatchingCriteria build(TransactionMonitoringSpec spec) {
         if (spec.getType() == TransactionIdentifierType.HASH) {
-            return new TxHashMatchingCriteria(spec.getNodeName(), spec.getTransactionIdentifier());
+            return new TxHashMatchingCriteria(spec.getNodeName(), spec.getTransactionIdentifier(), spec.getStatuses());
+        }
+
+        if (spec.getType() == TransactionIdentifierType.TO_ADDRESS) {
+            return new ToAddressMatchingCriteria(spec.getNodeName(), spec.getTransactionIdentifierValue(), spec.getStatuses());
+        }
+
+        if (spec.getType() == TransactionIdentifierType.FROM_ADDRESS) {
+            return new FromAddressMatchingCriteria(spec.getNodeName(), spec.getTransactionIdentifierValue(), spec.getStatuses());
         }
 
         throw new UnsupportedOperationException("Type: " + spec.getType() + " not currently supported");
