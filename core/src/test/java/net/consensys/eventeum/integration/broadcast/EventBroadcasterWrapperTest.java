4e8cc382918d5b6c46daa9a5d38268da3f7f482d
janayama <javier.anaya.martinez@gmail.com>
2019-10-15 08:29:05 +0200

    add test

---
+++ core/src/test/java/net/consensys/eventeum/integration/broadcast/EventBroadcasterWrapperTest.java
@@ -0,0 +1,43 @@
+package net.consensys.eventeum.integration.broadcast;
+
+import net.consensys.eventeum.dto.block.BlockDetails;
+import net.consensys.eventeum.integration.broadcast.blockchain.BlockchainEventBroadcaster;
+import net.consensys.eventeum.integration.broadcast.blockchain.EventBroadcasterWrapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+public class EventBroadcasterWrapperTest {
+
+    private static final Long EXPIRATION_MILLISECONDS = 6000000L;
+
+    private BlockchainEventBroadcaster blockchainEventBroadcaster;
+
+    @Before
+    public void init() {
+        blockchainEventBroadcaster = Mockito.mock(BlockchainEventBroadcaster.class);
+    }
+
+    @Test
+    public void testDisableBlockNotification() {
+        EventBroadcasterWrapper underTest = new EventBroadcasterWrapper(EXPIRATION_MILLISECONDS, blockchainEventBroadcaster, false);
+        final BlockDetails block = new BlockDetails();
+
+        underTest.broadcastNewBlock(block);PulsarBlockChainEventBroadcaster
+
+        verify(blockchainEventBroadcaster, never()).broadcastNewBlock(block);
+    }
+
+    @Test
+    public void testAllowBlockNotification() {
+        EventBroadcasterWrapper underTest = new EventBroadcasterWrapper(EXPIRATION_MILLISECONDS, blockchainEventBroadcaster, true);
+        final BlockDetails block = new BlockDetails();
+
+        underTest.broadcastNewBlock(block);
+
+        verify(blockchainEventBroadcaster).broadcastNewBlock(block);
+    }
+}
