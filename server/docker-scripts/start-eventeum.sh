ae28ffff91b93560cf7881254d96ce27069e8198
figuerascarlos <carlosd.figueras@gmail.com>
2019-11-13 12:51:24 +0100

    add support for jmx metrics for the jvm

--- server/docker-scripts/start-eventeum.sh
+++ server/docker-scripts/start-eventeum.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-command="java -jar eventeum-server.jar"
+command="java -Dcom.sun.management.jmxremote \
+              -Dcom.sun.management.jmxremote.port=1234 \
+              -Dcom.sun.management.jmxremote.ssl=false \
+              -Dcom.sun.management.jmxremote.authenticate=false \
+              -javaagent:./jmx_prometheus_javaagent.jar=8080:jmx-config.yml \
+              -jar eventeum-server.jar"
+
+if [[ -z JVM_OPS ]]; then
+  command="$command $JVM_OPS"
+fi
+
 if [[ -z CONF ]]; then
   command="$command --spring.config.additional-location=$CONF"
 fi
