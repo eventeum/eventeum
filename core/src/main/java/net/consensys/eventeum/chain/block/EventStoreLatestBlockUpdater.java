85e53064388c419e3051437f41aaa07c9f23a64e
janayama <javier.anaya.martinez@gmail.com>
2019-11-20 17:46:01 +0100

    add meterRegisrty into EventStoreLatestBlockUpdater

--- core/src/main/java/net/consensys/eventeum/chain/block/EventStoreLatestBlockUpdater.java
+++ core/src/main/java/net/consensys/eventeum/chain/block/EventStoreLatestBlockUpdater.java
@@ -1,11 +1,22 @@
+
 package net.consensys.eventeum.chain.block;
 
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
 import net.consensys.eventeum.chain.factory.BlockDetailsFactory;
+import net.consensys.eventeum.chain.service.container.ChainServicesContainer;
 import net.consensys.eventeum.chain.service.domain.Block;
 import net.consensys.eventeum.dto.block.BlockDetails;
 import net.consensys.eventeum.integration.eventstore.SaveableEventStore;
 import net.consensys.eventeum.model.LatestBlock;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * A contract event listener that saves the ContractEventDetails to a SaveableEventStore.
@@ -17,14 +28,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class EventStoreLatestBlockUpdater implements BlockListener {
 
     private SaveableEventStore saveableEventStore;
+    private MeterRegistry meterRegisrty;
 
     private BlockDetailsFactory blockDetailsFactory;
+    private Map<String ,AtomicLong> latestBlockMap;
 
     @Autowired
-    public EventStoreLatestBlockUpdater(
-            SaveableEventStore saveableEventStore, BlockDetailsFactory blockDetailsFactory) {
+    public EventStoreLatestBlockUpdater(SaveableEventStore saveableEventStore,BlockDetailsFactory blockDetailsFactory, MeterRegistry meterRegistry, ChainServicesContainer chainServicesContainer) {
         this.saveableEventStore = saveableEventStore;
+        this.meterRegisrty = meterRegistry;
+        this.latestBlockMap = new HashMap<>();
         this.blockDetailsFactory = blockDetailsFactory;
+
+        chainServicesContainer.getNodeNames().forEach( node -> {
+            this.latestBlockMap.put(node, meterRegistry.gauge(node +".latestBlock", Tags.of("chain",node),new AtomicLong(0)));
+        });
+
+
+
     }
 
     @Override
