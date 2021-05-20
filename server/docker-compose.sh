1b4f263546a31fffc8e984c7006e99c54b7c53df
xiagw <fxiaxiaoyu@gmail.com>
2021-05-20 14:29:51 +0800

    fix dockerfile

--- server/docker-compose.sh
+++ server/docker-compose.sh
@@ -1,34 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 #Thanks to Gregoire Jeanmart for this script
 echo "removing old containers"
-docker rm -f server_kafka_1
-docker rm -f server_zookeeper_1
-docker rm -f server_mongodb_1
-docker rm -f server_eventeum_1
-docker rm -f server_parity_1
 docker-compose down
 
 echo "removing storage"
-sudo rm -rf $HOME/mongodb/data
-sudo rm -rf $HOME/parity/data:/root/
-sudo rm -rf $HOME/parity/log
+sudo rm -rf "$HOME"/mongodb/data "$HOME"/parity/data/ "$HOME"/parity/log
 
-composescript="docker-compose.yml"
-
-if [ "$1" = "rinkeby" ]; then
+case "$1" in
+"rinkeby")
    composescript="docker-compose-rinkeby.yml"
    echo "Running in Rinkeby Infura mode..."
-elif [ "$1" = "infra" ]; then
+   ;;
+"infra")
    composescript="docker-compose-infra.yml"
    echo "Running in Infrastructure mode..."
-fi
+   ;;
+*)
+   composescript="docker-compose.yml"
+   ;;
+esac
 
-docker-compose -f "$composescript" build
-[ $? -eq 0 ] || exit $?;
+echo "Build..."
+docker-compose -f "$composescript" build || exit $?
 
-
-echo "Start"
-docker-compose -f "$composescript" up
-[ $? -eq 0 ] || exit $?;
+echo "Start..."
+docker-compose -f "$composescript" up -d || exit $?
 
 trap "docker-compose -f "$composescript" kill" INT
